038889f916c7b105e55869b9259b7b1660886517
Mayuri Bharti <mayuribharti43@gmail.com>
2026-04-20 15:15:05 +0530

    add Swagger annotations for API documentation

--- policy/src/main/java/com/insurance/policy/PolicyApplication.java
+++ policy/src/main/java/com/insurance/policy/PolicyApplication.java
@@ -1,8 +1,17 @@
 package com.insurance.policy;
 
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
+
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Policy Service API",
+		version = "1.0",
+		description = "API documentation for the Policy Service"
+	))
 @SpringBootApplication
 public class PolicyApplication {
 
--- policy/src/main/java/com/insurance/policy/controller/PolicyController.java
+++ policy/src/main/java/com/insurance/policy/controller/PolicyController.java
@@ -2,8 +2,10 @@ package com.insurance.policy.controller;
 
 import com.insurance.policy.entity.Policy;
 import com.insurance.policy.service.PolicyService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
@@ -20,36 +22,51 @@ public class PolicyController {
     }
 
     @PostMapping("/create")
+    @Operation(summary = "Create Policy",
+               description = "Create a new insurance policy")
     public Policy createPolicy(@RequestBody Policy policy){
         return policyService.createPolicy(policy);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get Policy by ID",
+               description = "Fetch a policy using its ID")
     public Policy getPolicyById(@PathVariable Long id) {
         return policyService.getPolicyById(id);
     }
 
     @GetMapping("/all")
+    @Operation(summary = "Get all policies",
+               description = "Fetch all policies from database")
     public List<Policy> getAllPolicies() {
         return policyService.getAllPolicies();
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete policy",
+               description = "Delete a policy by ID")
+
     public void deletePolicy(@PathVariable Long id){
         policyService.deletePolicy(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update policy (full)",
+               description = "Update entire policy details using PUT")
     public Policy updatePolicy(@PathVariable Long id, @RequestBody Policy policy){
         return policyService.updatePolicy(id,policy);
     }
 
     @PatchMapping("/{id}")
+    @Operation(summary = "Update policy (partial)",
+               description = "Partially update policy fields using PATCH")
     public Policy patchPolicy(@PathVariable Long id, @RequestBody Policy policy) {
         return policyService.patchPolicy(id, policy);
     }
 
-    @GetMapping("/filter}")
+    @GetMapping("/filter")
+    @Operation(summary = "Filter policies",
+               description = "Fetch policies by type and status")
     public List<Policy> filterPolicies(@RequestParam String type, @RequestParam String status) {
         return policyService.filterPolicies(type, status);
     }
